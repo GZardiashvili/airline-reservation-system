486700f1d9650e363797c499d635cd434838e74e
GZardiashvili <zardiashviligiorgi17@gmail.com>
2022-06-07 13:45:34 +0400

    FEAT: add express validation to login

--- backend/auth/login.js
+++ backend/auth/login.js
@@ -1,15 +1,23 @@
 const express = require('express');
 require('dotenv').config();
+const { body, validationResult } = require('express-validator');
 const authService = require('./services/authService');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', body('email').isEmail().normalizeEmail(), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+  }
   const user = await authService.loginUserWithJWT(
     req.body.email,
     req.body.password,
   );
-  res.status(200).send(user);
+  return res.status(200).send(user);
 });
 
 module.exports = router;
