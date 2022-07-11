62d2677deda17b575313ecacf1cab9c6524f2a89
GZardiashvili <zardiashviligiorgi17@gmail.com>
2022-07-12 03:10:01 +0400

    FIX: change more styles

--- frontend/src/app/layout/header/user-dialog/user-dialog.component.ts
+++ frontend/src/app/layout/header/user-dialog/user-dialog.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
